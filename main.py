6799cd55d504a64f8dd790198e34c4de1b0e3e13
Jack John <nwpigm5@gmail.com>
2021-03-20 06:24:01 +0000

    Added skill search

--- main.py
+++ main.py
@@ -38,7 +38,15 @@ async def on_message(message):
       await message.channel.send("".join(results))
   elif msg.startswith("!skills "):
     print("Asked for skills")
-    await message.channel.send("Skills search will be implemented at a later date.")
+    ship = msg.split("!skills ")[1]
+    skills = get_skills(ship)
+    if skills is None:
+      await message.channel.send("Error 404: Shipfu not found!")
+    else:
+      results = ["Here are the skills for {0}:\n\n".format(ship)]
+      for skill in skills:
+        results.append(skill[0] + ": " + skill[1] + "\n\n")
+      await message.channel.send("".join(results))
   elif msg.startswith("!"):
     print("Message was not a valid command")
     await message.channel.send("Invalid command!\n")
@@ -65,7 +73,29 @@ def get_stats(ship):
   return zip(stat_names, stat_values)
 
 def get_skills(ship):
-  return
+  response = requests.get("https://azurlane.koumakan.jp/" + ship)
+
+  if response.status_code == 200:
+    print("Successful get request!")
+  else:
+    print("Unsucessful get request!")
+    return None
+  
+  tables = response.text.split("mw-collapsible wikitable")[1]
+  unfiltered_text = "".join(tables)
+  skill_effects = []
+  for _ in re.findall("<td.*>([A-Z].*[\.|)])[ \n]*<", unfiltered_text):
+    if _ not in skill_effects:
+      skill_effects.append(_)
+  
+  skill_names = re.findall("<b>(.*)</b><br />", unfiltered_text)
+  for _ in range(len(skill_names)):
+    skill_names[_] = re.sub("<br />", " ", skill_names[_])
+
+  for skill in zip(skill_names, skill_effects):
+    print(skill[0], skill[1], sep=": ", end="\n\n")
+
+  return zip(skill_names, skill_effects)
 
 caffeinate()
 client.run(os.getenv("TOKEN"))
